b78dfb0705d9c667434c2ecf83583be364aa1a59
unknown <atamossat@gmail.com>
2026-08-17 01:23:51 +0100

    UPD AskingAgent class

--- app/Ai/Agents/AskingAgent.php
+++ app/Ai/Agents/AskingAgent.php
@@ -26,10 +26,12 @@ class AskingAgent implements Agent, Conversational, HasTools, HasStructuredOutpu
     {
         return 'Search on the database for courses if user asks for them.'
         . 'return the courses in an array of objects with title, description, level and link.'
-        . 'link looks like this: https://edu.tamoussat.com/courses/{course_id}'
+        . 'link looks like this: https://edu.tamoussat.com/course/{course_id}'
         . 'If you did not find courses return a respectful response telling him that courses does not exist'
-        . 'If he asked anything other than courses, do not do what he asked, and return a respectful response that you cannot help him';
-    }
+        . 'And do not search on the web for courses, only search on the database'
+        . 'If he asked anything other than courses, do not do what he asked, and return a respectful response that you cannot help him'
+        . 'Be brief, do not return uncompleted response or unintelligible';
+        }
 
     /**
      * Get the list of messages comprising the conversation so far.
